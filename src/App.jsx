9833cdcdeff820ac249170377e3ee65da67360b2
scr1geminihlc <scr1geminihlc@gmail.com>
2026-05-14 17:32:55 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -236,7 +236,13 @@ export default function App() {
       type="checkbox"
       className="w-7 h-7 rounded-full border-2 border-red-300 text-red-600 focus:ring-red-500 cursor-pointer"
       checked={logData?.tasks?.leave_day || false}
-      onChange={() => handleTaskToggle('leave_day')}
+<input
+      type="checkbox"
+      className="w-7 h-7 rounded-full border-2 border-red-300 text-red-600 focus:ring-red-500 cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed"
+      checked={logData?.tasks?.leave_day || false}
+      onChange={() => toggleTask('leave_day')}
+      disabled={role === 'supervisor'}
+    />
     />
   </div>
   <div>
